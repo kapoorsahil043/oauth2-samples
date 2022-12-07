dec10e5cb5a05da6cc55645b192b71f795904ce0
kapoorsahil043 <75875511+kapoorsahil043@users.noreply.github.com>
2022-12-07 16:58:28 +0800

    Create ResourceServerConfig.java

---
+++ spring-boot-auth-server-grant-password-inmem/src/main/java/com/sk/spring/cloud/security/ResourceServerConfig.java
@@ -0,0 +1,25 @@
+package com.sk.springcloud.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2").permitAll();
+
+                //.antMatchers("/hello")
+                //.hasRole("USER");
+
+                http.csrf().disable().headers().frameOptions().disable();
+    }
+}
